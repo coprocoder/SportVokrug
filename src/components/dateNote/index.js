a39560a05b2fcb1d53e811f16ae896c3fac04d4d
coprocoder <mog.ie@mail.ru>
2023-02-17 00:39:47 +0700

    feat: time range label

--- src/components/dateNote/index.js
+++ src/components/dateNote/index.js
@@ -5,16 +5,39 @@ const DateNote = ({dtStart = new Date().toISOString(), dtEnd = dtStart}) => {
   const [timeLabel, setTimeLabel] = useState(dtStart);
 
   useEffect(() => {
-    // TODO: check time range
-    setTimeLabel(isoToLocateDate());
+    setTimeLabel(genTimeLabel());
   }, [dtStart, dtEnd]);
 
-  function isoToLocateDate() {
-    let _date = new Date(dtStart);
-    const offsetMs = _date.getTimezoneOffset() * 60 * 1000;
-    const msLocal = _date.getTime() - offsetMs;
-    const dateLocal = new Date(msLocal);
-    return dateLocal.toLocaleDateString();
+  function genTimeLabel() {
+    const isoToLocaleDate = (isoDate) => {
+      const _date = new Date(isoDate);
+      const offsetMs = _date.getTimezoneOffset() * 60 * 1000;
+      const msLocal = _date.getTime() - offsetMs;
+      return new Date(msLocal);
+    };
+
+    let dateStart = isoToLocaleDate(dtStart);
+    let dateEnd = isoToLocaleDate(dtEnd);
+    let timeLabel = dateStart.toLocaleDateString();
+
+    if (dateStart !== dateEnd) {
+      let dateOptions = {};
+      if (dateStart.getMonth() - dateEnd.getMonth()) {
+        dateOptions = {
+          day: "numeric",
+          month: "long",
+        };
+      } else if (dateStart.getDay() - dateEnd.getDay()) {
+        dateOptions = {
+          day: "numeric",
+        };
+      }
+      const _dStart = dateStart.toLocaleDateString("ru-RU", dateOptions);
+      const _dEnd = dateEnd.toLocaleDateString("ru-RU");
+      timeLabel = `${_dStart}-${_dEnd}`;
+    }
+
+    return timeLabel;
   }
 
   return <div className="dateNote">{timeLabel}</div>;
